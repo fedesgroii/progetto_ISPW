8dad3033c7be093455b3a9104f100c8644006851
fedesgroii <federico.sgroi@students.uniroma2.eu>
2024-12-21 00:03:36 +0100

    Inizializzazione della configurazione di avvio utilizzando Singleton. Inoltre. è stata aggiunta la home page dell'utente "Paziente".

---
+++ src/main/java/StartupSettingsEntity.java
@@ -0,0 +1,41 @@
+public class StartupSettingsEntity {
+    // Istanza unica della classe (Singleton)
+    private static StartupSettingsEntity instance;
+
+    // Variabili per memorizzare le scelte di configurazione
+    private boolean colorMode; // Modalità a colori
+    private boolean saveToDatabase; // Salvataggio nel database
+
+    // Costruttore privato per impedire l'istanziazione diretta
+    private StartupSettingsEntity() {
+        // Impostazioni di default
+        this.colorMode = true; // Modalità a colori attiva di default
+        this.saveToDatabase = false; // Salvataggio disattivato di default
+    }
+
+    // Metodo statico per ottenere l'istanza unica della classe
+    public static StartupSettingsEntity getInstance() {
+        if (instance == null) {
+            instance = new StartupSettingsEntity();
+        }
+        return instance;
+    }
+
+    // Getter e setter per colorMode
+    public boolean isColorMode() {
+        return colorMode;
+    }
+
+    public void setColorMode(boolean colorMode) {
+        this.colorMode = colorMode;
+    }
+
+    // Getter e setter per saveToDatabase
+    public boolean isSaveToDatabase() {
+        return saveToDatabase;
+    }
+
+    public void setSaveToDatabase(boolean saveToDatabase) {
+        this.saveToDatabase = saveToDatabase;
+    }
+}
